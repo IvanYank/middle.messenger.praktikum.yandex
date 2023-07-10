b12c96e3a897acbe4c23d9496063f4356b75458b
Ivan <yanickiy.ivan@yandex.ru>
2023-07-10 15:57:19 +0400

    Создание сайта

---
+++ src/pages/chats/script.js
@@ -0,0 +1,7 @@
+const dialogCounts = document.querySelectorAll('.dialog__count');
+
+dialogCounts.forEach((item)=>{
+  if(item.innerText == 0){
+    item.style = "visibility:hidden"
+  }
+})
---
+++ src/pages/profile/script.js
@@ -0,0 +1,42 @@
+const profileStatic = document.querySelector('.profile__static-data');
+
+const changeAvatar = document.querySelector('.profile__avatar');
+const changeData = document.querySelector('.profile__change-data-button');
+const changePassword = document.querySelector('.profile__change-password-button');
+
+const changeAvatarWrap = document.querySelector('.profile__change-avatar-wrapper');
+const changeDataForm = document.querySelector('.profile__change-data');
+const changePasswordForm = document.querySelector('.profile__change-password');
+
+const changeDataSubmit = document.querySelector('.profile__change-data-submit');
+const changePasswordSubmit = document.querySelector('.profile__change-password-submit');
+
+changeAvatar.addEventListener('click', ()=>{
+  changeAvatarWrap.style="display:block"
+})
+
+changeAvatarWrap.addEventListener('click', (e)=>{
+  if(e.target.classList.contains('profile__change-avatar-wrapper')){
+    changeAvatarWrap.style="display:none;"
+  }
+})
+
+changeData.addEventListener('click', ()=>{
+  changeDataForm.style="display:flex"
+  profileStatic.style="display:none"
+})
+
+changeDataSubmit.addEventListener('click', ()=>{
+  changeDataForm.style="display:none"
+  profileStatic.style="display:flex"
+})
+
+changePassword.addEventListener('click', ()=>{
+  changePasswordForm.style="display:flex"
+  profileStatic.style="display:none"
+})
+
+changePasswordSubmit.addEventListener('click', ()=>{
+  changePasswordForm.style="display:none"
+  profileStatic.style="display:flex"
+})
