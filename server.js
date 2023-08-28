959157e90063c5a7aef53d92f539086bc344ef59
Ivan <yanickiy.ivan@yandex.ru>
2023-08-28 22:55:20 +0400

    Изменение server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from 'express';
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('./build'));
+app.use('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
-app.listen(PORT, () => { });
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
