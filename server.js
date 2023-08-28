31f9ed9280076f432df26531acb03a3b8329cb65
Ivan <yanickiy.ivan@yandex.ru>
2023-08-28 23:03:56 +0400

    Изменение server.js x3

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import path from 'path';
-// const express = require('express');
-// const path = require('path');
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static('./build'));
 app.use('/*', (req, res) => {
